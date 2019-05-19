f959e5855644439f6308d0bea75c7f5f6a72800c
Suleiman Ahamed Mamman <suleimamman@gmail.com>
2019-05-19 07:05:30 +0100

    Main Blog Javascript file created

--- resources/views/blogedit.blade.php
+++ resources/views/blogedit.blade.php
@@ -66,30 +66,5 @@
         </div>
       </section>
     
-<script type="text/javascript">
-  
-  function updateBlog(){
-    const form = document.getElementById("updateForm");
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You want update this blog",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, Update it!'
-    }).then((result) => {
-      if (result.value) {
-        form.submit();
-        Swal.fire(
-          '',
-          'Request Submited',
-          ''
-
-        )
-      }
-    });
-  }
-</script>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,6 +108,8 @@
 
 <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
 
+    <script src="{{ asset('js/main_blog.js') }}"></script>
+    
     <script src="{{ asset('js/jquery-3.2.1.min.j') }}s"></script>
     <script src="{{ asset('js/jquery-migrate-3.0.0.js') }}"></script>
     <script src="{{ asset('js/popper.min.js') }}"></script>
